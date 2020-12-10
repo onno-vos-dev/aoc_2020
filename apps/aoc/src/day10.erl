70a3e0086a5bea84a7053636ab7d477ba39a3968
Onno Vos <onno-vos-dev@users.noreply.github.com>
2020-12-11 00:47:06 +0100

    Cleanup of day10 and code golf

--- apps/aoc/src/day10.erl
+++ apps/aoc/src/day10.erl
@@ -4,9 +4,35 @@
 
 -export([ part_1/0
         , part_2/0
+        , golf/1
         ]).
 
-%%%_* Exported =================================================================
+%%%_* API ======================================================================
+
+golf(File) ->
+  {ok, Bin} = file:read_file(File),
+  Input = lists:sort([ binary_to_integer(S) || S <- binary:split(Bin, <<"\n">>, [trim, global]) ]),
+  FindF = fun(_F, [], _AccF, Acc) -> Acc;
+             (F, [A|T], AccF, Acc) ->
+              F(F, T, AccF, AccF(A, T, Acc))
+          end,
+  MapF = fun(Acc, I) ->
+             case Acc of
+               #{I := V} -> Acc#{I := V + 1};
+               #{} -> Acc#{I => 1}
+             end
+         end,
+  AccF1 = fun(A, B, {{X, Y}, {I, Acc}}) when hd(B) - A =:= 1 ->
+              {{X + 1, Y}, {I + 1, Acc}};
+             (A, B, {{X, Y}, {I, Acc}}) when hd(B) - A =:= 3 ->
+              {{X, Y + 1}, {0, MapF(Acc, I)}};
+             (_, _, {{X, Y}, {I, Acc}}) ->
+              {{X, Y}, {0, MapF(Acc, I)}}
+          end,
+  {{X, Y}, {_, #{2 := Twos, 3 := Threes, 4:= Fours}}} = FindF(FindF, Input, AccF1, {{1, 1}, {1, #{}}}),
+  A = X * Y,
+  B = round(math:pow(2, Twos) * math:pow(4, Threes) * math:pow(7, Fours)),
+  {A, B}.
 
 -spec part_1() -> integer().
 part_1() ->
@@ -31,12 +57,14 @@ part_2() ->
   {TwoOnes, ThreeOnes, FourOnes} = {maps:get(2, Acc), maps:get(3, Acc), maps:get(4, Acc)},
   round(math:pow(2, TwoOnes) * math:pow(4, ThreeOnes) * math:pow(7, FourOnes)).
 
+%%%_* Internal =================================================================
+-compile({inline, [find/3]}).
 find([], _AccF, Acc) -> Acc;
 find([A|T], AccF, Acc) ->
   find(T, AccF, AccF(A, T, Acc)).
 
 input() ->
-  util:read_file("day10.txt", <<"\n">>, fun(B) -> binary_to_integer(B) end).
+  util:read_file("day10.txt", <<"\n">>, fun erlang:binary_to_integer/1).
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
